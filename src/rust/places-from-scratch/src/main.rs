1f4ada3f4d3247269969d8c48bd5cb582653ae2a
Swagata Biswas <sbiswas@esri.com>
2024-11-06 16:17:33 +0000

    Add error handling

--- src/rust/places-from-scratch/src/main.rs
+++ src/rust/places-from-scratch/src/main.rs
@@ -1,22 +1,26 @@
 use std::env;
 
-async fn get_request_body() -> String {
-    let client = reqwest::Client::builder().build().unwrap();
+async fn get_request_body() -> Result<String, Box<dyn std::error::Error>> {
+    let client = reqwest::Client::builder().build()?;
 
-    let token = env::var("TOKEN").unwrap();
+    let token = env::var("TOKENNN")?;
 
     let url = format!("https://places-api.arcgis.com/arcgis/rest/services/places-service/v1/places/near-point?x=174.778&y=-41.292&pageSize=20&f=pjson&token={token}");
 
     let request = client.request(reqwest::Method::GET, url);
 
-    let response = request.send().await.unwrap();
-    response.text().await.unwrap()
+    let response = request.send().await?;
+    Ok(response.text().await?)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let body = get_request_body().await;
-    println!("{}", body);
+
+    match body {
+        Ok(result) => println!("{result}"),
+        Err(err) => println!("Something went wrong! Error: {err}"),
+    }
 
     Ok(())
 }
