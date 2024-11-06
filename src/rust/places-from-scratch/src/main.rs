445d855926d3ec5add5bd87013b56d2f6c70ef57
Swagata Biswas <sbiswas@esri.com>
2024-11-06 17:11:00 +0000

    Add parameters to function called in `main`

--- src/rust/places-from-scratch/src/main.rs
+++ src/rust/places-from-scratch/src/main.rs
@@ -1,24 +1,44 @@
 use std::{collections::HashMap, env};
 
-async fn get_request_body() -> Result<String, Box<dyn std::error::Error>> {
-    let client = reqwest::Client::builder().build()?;
+use reqwest::Client;
 
-    let token = env::var("TOKEN")?;
-
+async fn get_request_body(
+    client: &Client,
+    request_params: PlacesRequestParams,
+) -> Result<String, Box<dyn std::error::Error>> {
     let mut params = HashMap::new();
-    params.insert("x", "174.778");
-    params.insert("y", "-41.292");
-    params.insert("pageSize", "20");
-    params.insert("f", "pjson");
-    let request = client.get("https://places-api.arcgis.com/arcgis/rest/services/places-service/v1/places/near-point").query(&params).bearer_auth(token);
+    params.insert("x", request_params.x.to_string());
+    params.insert("y", request_params.y.to_string());
+    params.insert("pageSize", request_params.page_size.to_string());
+    params.insert("f", request_params.f);
+    let request = client.get("https://places-api.arcgis.com/arcgis/rest/services/places-service/v1/places/near-point").query(&params).bearer_auth(request_params.token);
 
     let response = request.send().await?;
     Ok(response.text().await?)
 }
 
+struct PlacesRequestParams {
+    x: f32,
+    y: f32,
+    page_size: u8,
+    f: String,
+    token: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let body = get_request_body().await;
+    let client = reqwest::Client::builder().build()?;
+    let token = env::var("TOKEN")?;
+
+    let request_params = PlacesRequestParams {
+        x: 174.778,
+        y: -41.292,
+        page_size: 20,
+        f: "pjson".to_string(),
+        token,
+    };
+
+    let body = get_request_body(&client, request_params).await;
 
     match body {
         Ok(result) => println!("{result}"),
