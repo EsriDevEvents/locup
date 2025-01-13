df4279427d26957b99e079f0b0f689a10b119b36
Jan Tschada <j.tschada@esri.de>
2025-01-13 11:56:10 +0100

    Added unit test for elevation service

--- src/rust/elevation/src/lib.rs
+++ src/rust/elevation/src/lib.rs
@@ -9,3 +9,25 @@ extern crate reqwest;
 
 pub mod apis;
 pub mod models;
+
+mod tests {
+    use apis::configuration::ApiKey;
+
+    use super::*;
+    use std::env;
+
+    #[tokio::test]
+    async fn test_get_elevation() {
+        // Check whether or not an ArcGIS API key was set using the environment
+        assert!(env::var("arcgis_api_key").is_ok());
+
+        // Create a default configuration
+        let mut configuration = apis::configuration::Configuration::default();
+        
+        // Define the API key
+        configuration.api_key = Some(apis::configuration::ApiKey {
+            prefix: None,
+            key: env::var("arcgis_api_key").expect("arcgis_api_key not set!")
+        });
+    }
+}
